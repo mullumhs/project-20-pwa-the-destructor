6307a08bb65d8d4b3cc8381925f84b140226fcbf
the-destructor <73207042+the-destructor@users.noreply.github.com>
2025-11-12 10:36:21 +1100

    created table table

--- models.py
+++ models.py
@@ -2,5 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    album = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    genre = db.Column(db.String(100))
